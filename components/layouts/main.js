e93300617876a5bd434a6b40071ab077f6047408
DreamFailor <sam.moillic@epitech.eu>
2023-11-28 14:16:07 +0100

    [FIX] Change format of the code

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,23 +1,23 @@
-import Head from 'next/head'
-import NavBar from '../navbar.js'
-import { Box, Container, MenuButton } from '@chakra-ui/react'
-import { HamburgerIcon } from '@chakra-ui/icons'
+import Head from "next/head"
+import NavBar from "../navbar.js"
+import { Box, Container, MenuButton } from "@chakra-ui/react"
+import { HamburgerIcon } from "@chakra-ui/icons"
 
-const Main = ({ children, router}) => {
-    return (
-        <Box as="main" pb={8}>
-            <Head>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <title>Sam Moillic - Homepage</title>
-            </Head>
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Sam Moillic - Homepage</title>
+      </Head>
 
-            <NavBar path={router.asPath} />
+      <NavBar path={router.asPath} />
 
-            <Container maxW="container.md" pt={14}>
-                {children}
-            </Container>
-        </Box>
-    )
+      <Container maxW="container.md" pt={14}>
+        {children}
+      </Container>
+    </Box>
+  )
 }
 
 export default Main
